88fcaa576a810f2e49611de06f349b9704c85c80
Jim <jlambert@hashicorp.com>
2021-11-19 17:14:02 -0500

    chore (Makefile): Add proto make targets with deps

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
+# Determine this makefile's path.
+# Be sure to place this BEFORE `include` directives, if any.
+THIS_FILE := $(lastword $(MAKEFILE_LIST))
+THIS_DIR := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 
-
+TMP_DIR := $(shell mktemp -d)
+REPO_PATH := github.com/hashicorp/db
 
 tools:
 	go generate -tags tools tools/tools.go
@@ -7,3 +12,29 @@ tools:
 
 fmt:
 	gofumpt -w $$(find . -name '*.go' | grep -v pb.go)
+
+
+### db tags requires protoc-gen-go v1.20.0 or later
+# GO111MODULE=on go get -u github.com/golang/protobuf/protoc-gen-go@v1.40
+proto: protolint protobuild
+
+protobuild:
+	# To add a new directory containing a proto pass the  proto's root path in
+	# through the --proto_path flag.
+	@bash scripts/protoc_gen_plugin.bash \
+		"--proto_path=internal/proto/local" \
+		"--proto_include_path=internal/proto/third_party" \
+		"--plugin_name=go" \
+		"--plugin_out=${TMP_DIR}"
+
+	# Move the generated files from the tmp file subdirectories into the current repo.
+	cp -R ${TMP_DIR}/${REPO_PATH}/* ${THIS_DIR}
+
+	@protoc-go-inject-tag -input=./internal/dbtest/dbtest.pb.go
+
+
+
+protolint:
+	@buf lint
+	# if/when this becomes a public repo, we can add this check
+	# @buf check breaking --against 'https://github.com/hashicorp/go-db.git#branch=main'
---
+++ internal/dbtest/timestamp.go
@@ -0,0 +1,65 @@
+package dbtest
+
+import (
+	"database/sql/driver"
+	"errors"
+	"math"
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// New constructs a new Timestamp from the provided time.Time.
+func New(t time.Time) *Timestamp {
+	return &Timestamp{
+		Timestamp: timestamppb.New(t),
+	}
+}
+
+// Now constructs a new Timestamp from the current time.
+func Now() *Timestamp {
+	return &Timestamp{
+		Timestamp: timestamppb.Now(),
+	}
+}
+
+// AsTime converts x to a time.Time.
+func (x *Timestamp) AsTime() time.Time {
+	return x.Timestamp.AsTime()
+}
+
+var (
+	NegativeInfinityTS = time.Date(math.MinInt32, time.January, 1, 0, 0, 0, 0, time.UTC)
+	PositiveInfinityTS = time.Date(math.MaxInt32, time.December, 31, 23, 59, 59, 1e9-1, time.UTC)
+)
+
+// Scan implements sql.Scanner for protobuf Timestamp.
+func (ts *Timestamp) Scan(value interface{}) error {
+	switch t := value.(type) {
+	case time.Time:
+		ts.Timestamp = timestamppb.New(t) // google proto version
+	case string:
+		switch value {
+		case "-infinity":
+			ts.Timestamp = timestamppb.New(NegativeInfinityTS)
+		case "infinity":
+			ts.Timestamp = timestamppb.New(PositiveInfinityTS)
+		}
+	default:
+		return errors.New("Not a protobuf Timestamp")
+	}
+	return nil
+}
+
+// Scan implements driver.Valuer for protobuf Timestamp.
+func (ts *Timestamp) Value() (driver.Value, error) {
+	if ts == nil {
+		return nil, nil
+	}
+	return ts.Timestamp.AsTime(), nil
+}
+
+// GormDataType gorm common data type (required)
+func (ts *Timestamp) GormDataType() string {
+	return "timestamp"
+}
